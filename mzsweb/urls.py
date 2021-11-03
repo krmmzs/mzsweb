f00ecf387fafec4154fbfba1d5a94a3b36ab26a9
mzs <mouzaisibyl@gmail.com>
2021-11-03 15:26:24 +0800

    create project structure

--- mzsweb/urls.py
+++ mzsweb/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('game.urls')), #gameapp is opened by default
+    #path('', include('game.urls')), #gameapp is opened by default
     path('admin/', admin.site.urls),
 ]
