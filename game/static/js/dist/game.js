fa4cbeb77461cae1d72656802ddfe41f7d610550
mzs <mouzaisibyl@gmail.com>
2021-11-04 17:12:33 +0800

    create menu

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -5,10 +5,25 @@ class MzsGameMenu
         this.root = root;
         this.$menu = $(`
 <div class="mzs-game-menu">
-    
+    <div class="mzs-game-menu-field">
+        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-single">
+            SinglePlayer
+        </div>
+        <br>
+        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-multi">
+            MultPlayer
+        </div>
+        <br>
+        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-settings">
+            Settings
+        </div>
+    </div>
 </div>
 `);
         this.root.$mzs_game.append(this.$menu);
+        this.$single = this.$menu.find('.mzs-game-menu-field-item-single');
+        this.$multi = this.$menu.find('.mzs-game-menu-field-item-multi');
+        this.$settings = this.$menu.find('.mzs-game-menu-field-item-settings');
     }
 }
 class MzsGame
