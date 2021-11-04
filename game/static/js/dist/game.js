95bb82bf4e03b6ead51daf9fe259e062c345375e
mzs <mouzaisibyl@gmail.com>
2021-11-04 18:56:48 +0800

    Button trigger JS

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -6,11 +6,11 @@ class MzsGameMenu
         this.$menu = $(`
 <div class="mzs-game-menu">
     <div class="mzs-game-menu-field">
-        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-single">
+        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-single-mode">
             SinglePlay
         </div>
         <br>
-        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-multi">
+        <div class="mzs-game-menu-field-item mzs-game-menu-field-item-multi-mode">
             MultPlay
         </div>
         <br>
@@ -21,8 +21,8 @@ class MzsGameMenu
 </div>
 `);
         this.root.$mzs_game.append(this.$menu);
-        this.$single = this.$menu.find('.mzs-game-menu-field-item-single');
-        this.$multi = this.$menu.find('.mzs-game-menu-field-item-multi');
+        this.$single_mode = this.$menu.find('.mzs-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.mzs-game-menu-field-item-multi-mode');
         this.$settings = this.$menu.find('.mzs-game-menu-field-item-settings');
 
         this.start();
@@ -30,7 +30,21 @@ class MzsGameMenu
 
     start()
     {
-        
+        this.add_listening_events();
+    }
+
+    add_listening_events()
+    {
+        let outer = this;
+        this.$single_mode.click(function(){
+            console.log("click single mode");
+        });
+        this.$multi_mode.click(function(){
+            console.log("click multi mode");
+        });
+        this.$settings.click(function(){
+            console.log("click settings");
+        });
     }
 
 
