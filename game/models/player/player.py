11909c7fbb0b52058b15a9c88508d3d55c3d08f6
mzs <mouzaisibyl@gmail.com>
2021-11-19 15:13:43 +0800

    create a model about player that hava a head portrait

---
+++ game/models/player/player.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE) #希望我们的表是从user表来扩充的, 当user删除的时候,删除关联的player
+    photo = models.URLField(max_length=256, blank = True) #加入头像
+
+    def __str__(self):
+        return str(self.user)
