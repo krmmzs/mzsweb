2d28b90cadd3f7b62effb55ed7090befc8f49a99
mzs <mouzaisibyl@gmail.com>
2021-11-24 14:56:02 +0800

    add openid

--- game/models/player/player.py
+++ game/models/player/player.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE) #希望我们的表是从user表来扩充的, 当user删除的时候,删除关联的player
     photo = models.URLField(max_length=256, blank = True) #加入头像
-
+    openid = models.CharField(default="", max_length=50, blank=True, null=True) # 一键登录的openid
     def __str__(self):
         return str(self.user)
--- mzsweb/settings.py
+++ mzsweb/settings.py
@@ -82,6 +82,16 @@ DATABASES = {
     }
 }
 
+CACHES = { 
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },  
+    },  
+}
+USER_AGENTS_CACHE = 'default'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
