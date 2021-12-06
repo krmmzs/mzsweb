04ea903464db7876eb5d281f896de5897034ac73
mzs <mouzaisibyl@gmail.com>
2021-12-06 21:01:38 +0800

    .

--- game/views/settings/acwing/acapp/receive_code.py
+++ game/views/settings/acwing/acapp/receive_code.py
@@ -1,18 +1,28 @@
-from django.shortcuts import redirect
 from django.core.cache import cache
 import requests
 from django.contrib.auth.models import User
 from game.models.player.player import Player
-from django.contrib.auth import login
 from random import randint
+from django.http import JsonResponse
 
 def receive_code(request):
     data = request.GET
+
+    if "errcode" in data:
+        return JsonResponse({
+            'result': "apply failed",
+            'errcode': data['errcode'],
+            'errmsg': data['errmsg'],
+        })
+
+
     code = data.get('code')
     state = data.get('state')
 
     if not cache.has_key(state):
-        return redirect("index")
+        return JsonResponse({
+            'result': "state not exist"
+        })
     cache.delete(state)
 
     apply_access_token_url = "https://www.acwing.com/third_party/api/oauth2/access_token/"
@@ -29,8 +39,13 @@ def receive_code(request):
 
     players = Player.objects.filter(openid=openid)
     if players.exists():  # 如果该用户已存在，则无需重新获取信息，直接登录即可
-        login(request, players[0].user)
-        return redirect("index")  # This is where the names of urls are finally used, so you could not ues Ajax to write a ura
+        player = players[0]
+        return JsonResponse({
+            'result': "success",
+            'username': player.user.username,
+            'photo': player.photo,
+        })
+
 
     get_userinfo_url = "https://www.acwing.com/third_party/api/meta/identity/getinfo/"
     params = {
@@ -47,8 +62,8 @@ def receive_code(request):
     user = User.objects.create(username=username)
     player = Player.objects.create(user=user, photo=photo, openid=openid)
 
-    login(request, user)
-
-    return redirect("index")
-
-
+    return JsonResponse({
+        'result': "success",
+        'username': player.user.username,
+        'photo': player.photo,
+    })
--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -114,8 +114,15 @@ class Settings
 
     start()
     {
-        this.getinfo();
-        this.add_listening_events();
+        if(this.platform === "ACAPP")
+        {
+            this.getinfo_acapp();
+        }
+        else
+        {
+            this.getinfo_web();
+            this.add_listening_events();
+        }
     }
 
     add_listening_events()
@@ -262,9 +269,38 @@ class Settings
         this.$register.show();
     }
 
+    acapp_login(appid, redirect_uri, scope, state)
+    {
+        let outer = this;
+        this.root.acos.api.oauth2.authorize(appid, redirect_uri, scope, state, function(resp) {
+            if(resp.result === "success")
+            {
+                outer.username = resp.username;
+                outer.photo = resp.photo;
+                outer.hide();
+                outer.root.menu.show();
+            }
+        });
+    }
 
+    getinfo_acapp()
+    {
+        let outer = this;
 
-    getinfo()
+        $.ajax({
+            url: "https://app188.acapp.acwing.com.cn/settings/acwing/acapp/apply_code/",
+            type: "GET",
+            success: function(resp)
+            {
+                if(resp.result === "success")
+                {
+                    outer.acapp_login(resp.appid, resp.redirect_uri, resp.scope, resp.state);
+                }
+            }
+        });
+    }
+
+    getinfo_web()
     {
         let outer = this;
 
