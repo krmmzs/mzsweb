59130ddcb974c2f96581e236c45de6c80034cfd1
mzs <mouzaisibyl@gmail.com>
2021-12-09 16:50:28 +0800

    random number uuid

--- static/js/src/playground/1mzs_game_object/zbase.js
+++ static/js/src/playground/1mzs_game_object/zbase.js
@@ -8,6 +8,21 @@ class MzsGameObject
 
         this.has_called_start = false; // 是否执行过start函数
         this.timedelta = 0; // 当前帧距离上一帧的时间间隔
+        this.uuid = this.create_uuid();
+
+        console.log(this.uuid);
+    }
+
+    create_uuid()
+    {
+        let res = "";
+        for(let i = 0; i < 20; i ++)
+        {
+            let x = parseInt(Math.floor(Math.random() * 10));
+            res += x;
+        }
+        return res;
+
     }
 
     start() // 只会在第一次执行
